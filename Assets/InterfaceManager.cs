9952429e3d6438983c7bc19fac56287b5daff3c5
Everi Osofsky <eosofsky@andrew.cmu.edu>
2015-10-20 19:25:20 -0400

    Added canvas to environment

---
+++ Assets/InterfaceManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceManager : MonoBehaviour {
+
+	public GameObject canvas;
+	private bool canvasOpen;
+	
+	// Use this for initialization
+	void Start () {
+		canvas.SetActive (false);
+		canvasOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Submit")) {
+			if (canvasOpen) {
+				canvas.SetActive(false);
+				canvasOpen = false;
+			}
+			else {
+				canvas.SetActive(true);
+				canvasOpen = true;
+			}
+		}
+	}
+}
--- Assets/Scripts/DrawingManager.cs
+++ Assets/Scripts/DrawingManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DrawingManager : MonoBehaviour
 {
 	private bool isMousePressed;
+	private bool initial;
 	int canvasMask;
 	float camRayLength = 100f;
 	Texture2D texture;
@@ -11,22 +12,35 @@ public class DrawingManager : MonoBehaviour
 	public Transform topRight;
 	private float widthFactor;
 	private float heightFactor;
-	
+	private Vector2 lastpoint;
+
+	void drawPoint(Vector2 point) {
+		int x = (int) point.x;
+		int y = (int) point.y;
+		texture.SetPixel(x-1, y-1, Color.black);
+		texture.SetPixel(x-1, y, Color.black);
+		texture.SetPixel(x-1, y+1, Color.black);
+		texture.SetPixel(x, y-1, Color.black);
+		texture.SetPixel(x, y, Color.black);
+		texture.SetPixel(x, y+1, Color.black);
+		texture.SetPixel(x+1, y-1, Color.black);
+		texture.SetPixel(x+1, y, Color.black);
+		texture.SetPixel(x+1, y+1, Color.black);
+	}
+
 	void Awake ()
 	{	
 		isMousePressed = false;
 		
 		canvasMask = LayerMask.GetMask ("Canvas");
 
-		texture = new Texture2D(200, 200);
+		texture = new Texture2D(500, 300);
 		GetComponent<Renderer>().material.mainTexture = texture;
 		
-		for (int y = 0; y < texture.height; y++) {
-			for (int x = 0; x < texture.width; x++) {
-				texture.SetPixel(x, y, Color.white);
-			}
-		}
-		texture.Apply();
+		clear ();
+
+		lastpoint = new Vector2 (0,0);
+		initial = true;
 
 		widthFactor = (texture.width / (topRight.position.x - bottomLeft.position.x));
 		heightFactor = (texture.height / (topRight.position.y - bottomLeft.position.y));
@@ -40,6 +54,7 @@ public class DrawingManager : MonoBehaviour
 		
 		if (Input.GetMouseButtonUp (0)) {
 			isMousePressed = false;
+			initial = true;
 		}
 		
 		if (isMousePressed) {
@@ -50,15 +65,26 @@ public class DrawingManager : MonoBehaviour
 				Vector3 hitPoint = canvasHit.point;
 				int x = (int) ((hitPoint.x - bottomLeft.position.x) * widthFactor);
 				int y = (int) ((hitPoint.y - bottomLeft.position.y) * heightFactor);
-				texture.SetPixel(x-1, y-1, Color.black);
-				texture.SetPixel(x-1, y, Color.black);
-				texture.SetPixel(x-1, y+1, Color.black);
-				texture.SetPixel(x, y-1, Color.black);
-				texture.SetPixel(x, y, Color.black);
-				texture.SetPixel(x, y+1, Color.black);
-				texture.SetPixel(x+1, y-1, Color.black);
-				texture.SetPixel(x+1, y, Color.black);
-				texture.SetPixel(x+1, y+1, Color.black);
+				Vector2 newPoint = new Vector2 (x, y);
+				drawPoint (newPoint);
+
+				if (initial) {
+					lastpoint = new Vector2 (newPoint.x, newPoint.y);
+					initial = false;
+				}
+				else {
+					for (float i = 0.0f; i < 1.0f; i+=0.02f) {
+						drawPoint(Vector2.Lerp(lastpoint, newPoint,i));
+					}
+				}
+
+				
+				//				while (lastpoint != newPoint) {
+//					drawPoint (lastpoint);
+//					lastpoint = Vector2.Lerp(lastpoint, newPoint, 0.5f);
+//				}
+				lastpoint = newPoint;
+
 				texture.Apply();
 			}
 		}
@@ -79,7 +105,19 @@ public class DrawingManager : MonoBehaviour
 
 	}
 	
-	
+	public void clear () {
+		for (int y = 0; y < texture.height; y++) {
+			for (int x = 0; x < texture.width; x++) {
+				texture.SetPixel(x, y, Color.white);
+			}
+		}
+		texture.Apply();
+	}
+
+	void OnEnable () {
+		clear ();
+	}
+
 }
 
 
