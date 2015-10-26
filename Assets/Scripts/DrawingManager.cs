e883f9eb32755ae861f67b5cb8e1cc5dd9528fb5
Christine Lee <chris.yx.lee@gmail.com>
2015-10-26 16:59:32 -0400

    Code for reading in accelerations from wiimote

--- Assets/Scripts/DrawingManager.cs
+++ Assets/Scripts/DrawingManager.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawingManager : MonoBehaviour
 {
+	//for Drawing
 	private bool isMousePressed;
 	private bool initial;
 	int canvasMask;
@@ -13,7 +17,15 @@ public class DrawingManager : MonoBehaviour
 	private float widthFactor;
 	private float heightFactor;
 	private Vector2 lastpoint;
-
+	
+	//for Wiimote
+	int calY = 100;
+	int calX = -100;
+	int smooth = 20;
+	int lastLineWiiMote;
+	double oldAccX = 0, oldAccY = 0, oldAccZ = 0;
+	double accX = 0, accY = 0, accZ = 0;
+	
 	void drawPoint(Vector2 point) {
 		int x = (int) point.x;
 		int y = (int) point.y;
@@ -27,27 +39,79 @@ public class DrawingManager : MonoBehaviour
 		texture.SetPixel(x+1, y, Color.black);
 		texture.SetPixel(x+1, y+1, Color.black);
 	}
-
+	
 	void Awake ()
 	{	
 		isMousePressed = false;
 		
 		canvasMask = LayerMask.GetMask ("Canvas");
-
+		
 		texture = new Texture2D(500, 300);
 		GetComponent<Renderer>().material.mainTexture = texture;
 		
 		clear ();
-
+		
 		lastpoint = new Vector2 (0,0);
 		initial = true;
-
+		
 		widthFactor = (texture.width / (topRight.position.x - bottomLeft.position.x));
 		heightFactor = (texture.height / (topRight.position.y - bottomLeft.position.y));
 	}
 	
 	void Update()
 	{
+		/* 
+		 * To use Wiimote, person should run DarwiinRemote and start recording into a file called
+		 * "wiimote.txt" in the SceneStudio directory. Once that is up and running, run this file
+		 * and the program will start reading coordinates from there.
+		 */
+		string coordinates = "wiimote.txt";
+		string coordinateFile = System.IO.Path.GetFullPath (coordinates);
+		
+		string[] lines;
+		string[][] values;
+		
+		UnityEngine.Debug.Log (coordinateFile);
+		
+		//gets the coordinates from the file outputted by DarwiinRemote
+		if (System.IO.File.Exists(coordinateFile)){ 
+			string txt;
+			
+			using (StreamReader sr = new StreamReader(coordinateFile)) {
+				//read two lines for the labels
+				sr.ReadLine();
+				sr.ReadLine();
+				//get the updated information
+				txt = sr.ReadToEnd();
+				lines = txt.Split(new string[] { "\n" }, StringSplitOptions.None);
+				values = new string[lines.Length][];
+				for (int i = 0; i < lines.Length; i++) {
+					values[i] = lines[i].Split(new string[] { "," }, StringSplitOptions.None);
+				}
+				
+				double tempX = double.Parse(values[values.Length-2][1],CultureInfo.InvariantCulture);
+				double tempY = double.Parse(values[values.Length-2][2],CultureInfo.InvariantCulture);
+				double tempZ = double.Parse(values[values.Length-2][3],CultureInfo.InvariantCulture);
+				
+				if (tempX == oldAccX && tempY == oldAccY && tempZ == oldAccZ) {
+					//no change, the wiimote probably disconnected
+					accX = 0; accY = 0; accZ = 0;
+				} else {
+					//changed, so update to the new accelerations
+					oldAccX = accX; oldAccY = accY; oldAccZ = accZ;
+					accX = tempX; accY = tempY; accZ = tempZ;
+				}
+				
+				UnityEngine.Debug.Log (accX + " " + accY + " " + accZ);
+			}
+			
+			//			using (StreamWriter sw = new StreamWriter(coordinates, false)) {
+			//				//refresh so that we won't read it in again next time
+			//				sw.WriteLine("time,AccX,AccY,AccZ,pressureTR, pressureBR, pressureTL,");
+			//				sw.WriteLine("pressureBL,rawPressureTR,rawPressureBR,rawPressureTL,rawPressureBL");
+			//			}
+		}
+
 		if (Input.GetMouseButtonDown (0)) {
 			isMousePressed = true;
 		}
