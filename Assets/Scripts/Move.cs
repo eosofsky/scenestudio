4ca50836afdfe0e09ed99bd9e8bbd41d5079976b
Everi Osofsky <eosofsky@andrew.cmu.edu>
2015-11-14 18:09:24 -0500

    Added movement script

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 0.5f;
+	private Camera thisCamera;
+
+	void Awake () {
+		thisCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		Vector3 newPos = Vector3.Lerp (transform.position, transform.position + (h * thisCamera.transform.right) + (v * thisCamera.transform.forward), speed);
+		if (newPos.y >= 0)
+			//newPos.y = transform.position//0;
+			transform.position = newPos;
+	}
+}
