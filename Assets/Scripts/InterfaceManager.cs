7f1ed533f1fa6f5acfffa3bdd3a62553db4119c7
js12815 <jshen12815>
2015-11-28 05:36:42 -0800

    Basic UI

--- Assets/Scripts/InterfaceManager.cs
+++ Assets/Scripts/InterfaceManager.cs
@@ -5,10 +5,25 @@ public class InterfaceManager : MonoBehaviour {
 
 	public GameObject canvas;
 	private bool canvasOpen;
+
+	public GameObject normalState;  //no canvas
+	public GameObject drawingState; //canvas up
+	public GameObject pickState; //add model?
+	public GameObject wrongState; //what model did you mean?
+	public GameObject addState; //add selected model??
+	public GameObject systemState; //drawingsystem
+	
 	
 	// Use this for initialization
 	void Start () {
 		canvas.SetActive (false);
+		normalState.SetActive (true);
+		drawingState.SetActive (false);
+		pickState.SetActive (false);
+		wrongState.SetActive (false);
+		addState.SetActive (false);
+		systemState.SetActive (false);
+		
 		canvasOpen = false;
 	}
 
@@ -29,4 +44,77 @@ public class InterfaceManager : MonoBehaviour {
 			Toggle ();
 		}
 	}
+	//pull up canvas when draw button is clicked
+	public void draw() {
+		canvas.SetActive (true);
+		canvasOpen = true;
+		normalState.SetActive (false);
+		pickState.SetActive (false);
+		drawingState.SetActive (true);
+		systemState.SetActive (true);
+	}
+	
+	//call algorithm, open add state
+	public void doneDrawing() {
+		//loading state?
+		wrongState.SetActive (false);
+		pickState.SetActive (true);
+		drawingState.SetActive (false);
+		canvas.SetActive (false);
+		systemState.SetActive (true);
+	}
+
+
+	
+	//either doesnt recognize, or player says model is wrong
+	public void wrongModel() {
+		wrongState.SetActive (true);
+		drawingState.SetActive (false);
+		pickState.SetActive (false);
+		addState.SetActive (false);
+		systemState.SetActive (true);
+	}
+
+	//confirm new model selection
+	public void newModel() {
+		addState.SetActive (true);
+		wrongState.SetActive (false);
+		systemState.SetActive (true);
+	}
+	
+	//sets back to normal state
+	public void close() {
+		canvas.SetActive (false);
+		normalState.SetActive (true);
+		drawingState.SetActive (false);
+		pickState.SetActive (false);
+		wrongState.SetActive (false);
+		addState.SetActive (false);
+		systemState.SetActive (false);
+		
+	}
 }
+
+
+
+	
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
